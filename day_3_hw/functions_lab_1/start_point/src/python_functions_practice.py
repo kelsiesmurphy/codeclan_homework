fdbb185f874c5510eea66d03d45ba0410e6d467c
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2022-11-09 16:20:13 +0000

    Homework Submitted

---
+++ day_3_hw/functions_lab_1/start_point/src/python_functions_practice.py
@@ -0,0 +1,67 @@
+def return_10():
+    return 10
+
+def add(num_1, num_2):
+    return num_1 + num_2
+
+def subtract(num_1, num_2):
+    return num_1 - num_2
+
+def multiply(num_1, num_2):
+    return num_1 * num_2
+
+def divide(num_1, num_2):
+    return num_1 / num_2
+
+def length_of_string(string):
+    return len(string)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(string_1, string_2):
+    return int(string_1) + int(string_2)
+
+def number_to_full_month_name(month_number):
+    months = {
+        1: "January",
+        2: "February",
+        3: "March",
+        4: "April",
+        5: "May",
+        6: "June",
+        7: "July",
+        8: "August",
+        9: "September",
+        10: "October",
+        11: "November",
+        12: "December",
+        }
+    return months[month_number]
+
+def number_to_short_month_name(month_number):
+    months = {
+        1: "Jan",
+        2: "Feb",
+        3: "Mar",
+        4: "Apr",
+        5: "May",
+        6: "Jun",
+        7: "Jul",
+        8: "Aug",
+        9: "Sep",
+        10: "Oct",
+        11: "Nov",
+        12: "Dec",
+        }
+    return months[month_number]
+
+
+def cube_volume(length):
+    return length ** 3
+
+def random_string(string):
+    return string[::-1]
+
+def temp_conversions(temp):
+    return (temp - 30) /2
