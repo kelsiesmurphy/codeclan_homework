97c88e2c08eec7cb09c8e0ff41bde39b3ae6e662
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2022-11-11 13:32:49 +0000

    Completed homework

--- week_01/weekend_homework/start_point/src/pet_shop.py
+++ week_01/weekend_homework/start_point/src/pet_shop.py
@@ -1,1 +1,76 @@
 # WRITE YOUR FUNCTIONS HERE
+def get_pet_shop_name(pet_shop):
+    return pet_shop["name"]
+
+def get_total_cash(pet_shop):
+    return pet_shop["admin"]["total_cash"]
+
+def add_or_remove_cash(pet_shop, cash_value):
+    pet_shop["admin"]["total_cash"] += cash_value
+    return pet_shop["admin"]["total_cash"]
+
+def get_pets_sold(pet_shop):
+    return pet_shop["admin"]["pets_sold"]
+
+def increase_pets_sold(pet_shop, pets_sold):
+    pet_shop["admin"]["pets_sold"] += pets_sold
+    return pet_shop["admin"]["pets_sold"]
+
+def get_stock_count(pet_shop):
+    return len(pet_shop["pets"])
+
+def get_pets_by_breed(pet_shop, breed):
+    pets_by_breed = []
+    for pet in pet_shop["pets"]:
+        if pet["breed"] == breed:
+            pets_by_breed.append(pet)
+    return pets_by_breed
+
+def find_pet_by_name(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            return pet
+
+def remove_pet_by_name(pet_shop, pet_name):
+    for pet in pet_shop["pets"]:
+        if pet["name"] == pet_name:
+            pet_shop["pets"].remove(pet)
+
+def add_pet_to_stock(pet_shop, new_pet):
+    return pet_shop["pets"].append(new_pet)
+
+def get_customer_cash(customer):
+    return customer["cash"]
+
+def remove_customer_cash(customer, cash_value):
+    customer["cash"] -= cash_value
+    return customer["cash"]
+
+def get_customer_pet_count(customer):
+    return len(customer["pets"])
+
+def add_pet_to_customer(customer, new_pet):
+    return customer["pets"].append(new_pet)
+
+
+    # --- OPTIONAL ---
+
+
+def customer_can_afford_pet(customer, new_pet):
+    if customer["cash"] >= new_pet["price"]:
+        return True
+    else:
+        return False
+
+
+    # --- Integration Tests ---
+
+def sell_pet_to_customer(pet_shop, pet, customer):
+    if pet in pet_shop["pets"] and customer["cash"] >=pet["price"]:
+        customer["cash"] -= pet["price"]
+        pet_shop["admin"]["total_cash"] += pet["price"]
+        pet_shop["admin"]["pets_sold"] += 1
+        pet_shop["pets"].remove(pet)
+        customer["pets"].append(pet)
+    else:
+        pass
