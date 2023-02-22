a4091029e0c3e14c4e8d2ba97da4de2bca342758
Kelsie Murphy <kelsie@kelsiesmurphy.com>
2023-02-22 16:22:51 +0000

    Adding homework completion

--- week_14/day_03/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
+++ week_14/day_03/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/DistilleryController.java
@@ -1,10 +1,32 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class DistilleryController {
+    @Autowired
+    DistilleryRepository distilleryRepository;
 
-
+    @GetMapping(value = "/distilleries")
+    public ResponseEntity<List<Distillery>> getAllDistilleries(@RequestParam Optional<String> region, @RequestParam Optional<Integer> whiskiesAge) {
+        if (region.isPresent()) {
+            return new ResponseEntity<>(distilleryRepository.findByRegion(region.get()), HttpStatus.OK);
+        } else if (whiskiesAge.isPresent()) {
+            return new ResponseEntity<>(distilleryRepository.findByWhiskiesAge(whiskiesAge.get()), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(distilleryRepository.findAll(), HttpStatus.OK);
+    }
 }
--- week_14/day_03/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
+++ week_14/day_03/WhiskyTracker_start/src/main/java/com/codeclan/example/WhiskyTracker/controllers/WhiskyController.java
@@ -1,9 +1,38 @@
 package com.codeclan.example.WhiskyTracker.controllers;
 
+import com.codeclan.example.WhiskyTracker.models.Distillery;
+import com.codeclan.example.WhiskyTracker.models.Whisky;
+import com.codeclan.example.WhiskyTracker.repositories.DistilleryRepository;
+import com.codeclan.example.WhiskyTracker.repositories.WhiskyRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.text.html.Option;
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class WhiskyController {
+    @Autowired
+    WhiskyRepository whiskyRepository;
 
+    @Autowired
+    DistilleryRepository distilleryRepository;
+
+    @GetMapping(value = "/whiskies")
+    public ResponseEntity<List<Whisky>> getAllWhiskies(@RequestParam Optional<Integer> year, @RequestParam Optional<String> distilleryName, @RequestParam Optional<Integer> age, @RequestParam Optional<String> distilleryRegion) {
+        if(year.isPresent()){
+            return new ResponseEntity<>(whiskyRepository.findByYear(year.get()), HttpStatus.OK);
+        } else if (distilleryName.isPresent() && age.isPresent()) {
+            return new ResponseEntity<>(whiskyRepository.findByDistilleryNameAndAge(distilleryName.get(), age.get()), HttpStatus.OK);
+        } else if (distilleryRegion.isPresent()) {
+            return new ResponseEntity<>(whiskyRepository.findByDistilleryRegion(distilleryRegion.get()), HttpStatus.OK);
+        }
+        return new ResponseEntity<>(whiskyRepository.findAll(), HttpStatus.OK);
+    }
 }
